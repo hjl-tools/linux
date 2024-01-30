6104536054e5c164e2727648ae4c9d514d98fab5
H.J. Lu <hjl.tools@gmail.com>
2024-01-30 11:42:31 -0800

    Update shared-object.c

--- arch/x86/kernel/shared-object.c
+++ arch/x86/kernel/shared-object.c
@@ -2,6 +2,8 @@
 #include <linux/init.h>
 #include <linux/printk.h>
 
+#include <linux/netfilter/x_tables.h>
+
 static int __init module_a_init(void)
 {
     printk(KERN_INFO "Module A init\n");
@@ -9,7 +11,9 @@ static int __init module_a_init(void)
 }
 int module_a_function(void) 
 {
-       printk(KERN_INFO "Module A function\n");
+          if (static_key_false(&xt_tee_enabled)){
+                 printk(KERN_INFO "test\n");
+          }
        return 0;
 }
 EXPORT_SYMBOL_GPL(module_a_function);
@@ -17,6 +21,7 @@ static void __exit module_a_exit(void)
 {
        printk(KERN_INFO "Module A exit\n");
 }
+
 module_init(module_a_init);
 module_exit(module_a_exit);
 MODULE_LICENSE("GPL");
